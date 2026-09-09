efee1db47baab89970efb668c8c776feb71940fc
tornike96 <tokoeqizashvili2486@gmail.com>
2026-09-09 18:15:59 +0400

    Implement number guessing game logic and add difficulty selection

--- main.py
+++ main.py
@@ -1,5 +1,78 @@
+import random
+
+WELCOME_TEXT = "Welcome to the Number Guessing Game!\n" \
+    "I'm thinking of a number between 1 and 100.\n" \
+    "You have X chances to guess the correct number."
+
+DIFFICULTY_INTRUCTION = "Please select the difficulty level:\n" \
+    "1. Easy (10 chances)\n" \
+    "2. Medium (5 chances)\n" \
+    "3. Hard (3 chances)\n"
+    
+# RANDOM_NUMBER = random.randint(1, 100)
+RANDOM_NUMBER = 5
+
+def choose_difficulty() -> str:
+    print(DIFFICULTY_INTRUCTION)
+    while True:
+        user_input = input("Enter your choice: ")
+        if user_input not in ['1', '2', '3']:
+            print("\nInvalid input. Please choose '1', '2' or '3'.\n")
+        else:
+            break
+    
+    if user_input == '1':
+        return ('Easy', 10)
+    elif user_input == '2':
+        return ('Medium', 5)
+    else:
+        return ('Hard', 3)
+
+def play_game() -> None:
+    attempt_count = 0
+    difficulty = choose_difficulty()
+    
+    print(f"\nGreat! You have selected the {difficulty[0]} difficulty level.\n" \
+            "Let's start the game!")
+    
+    while True:
+        if attempt_count >= difficulty[1]:
+            print('\nSorry, you have run out of attempts.')
+            break
+        player_guess = input('\nEnter your guess: ')
+        if player_guess.isdigit():
+            attempt_count += 1
+            if int(player_guess) > RANDOM_NUMBER:
+                print(f"Incorrect! The number is less than {player_guess}")
+            elif int(player_guess) < RANDOM_NUMBER:
+                print(f"Incorrect! The number is greater than {player_guess}")
+            else:
+                print(f"Congratulations! You guessed the correct number in {attempt_count} attempts.")
+                break
+        else:
+            print("Please, choose a positive integer.")
+
+
+def game() -> None:
+    while True:
+        play_game()
+        
+        while True:
+            user_decision = input("\nDo you want to play the game again? Press Y/N ")
+            print()
+            if user_decision.lower() in ('y', 'n'):
+                break
+            else:
+                print("Please type and submit N or Y.")
+        
+        if user_decision == 'n':
+            break
+                
+                
 def main():
-    print("Hello from guess-num!")
+    print(WELCOME_TEXT, "\n")
+    
+    game()
 
 
 if __name__ == "__main__":
